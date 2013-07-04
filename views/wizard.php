bc31d53c7135f348e4cdb2bf104555caf6196317
Leon Rowland <leon@rowland.nl>
2013-07-04 15:16:53 +0200

    missing wrapper and page title

--- views/wizard.php
+++ views/wizard.php
@@ -1,10 +1,13 @@
-<?php $wizard_names = Pronamic_WordPress_ConfigurationWizard_Factory::get_all_registered_wizards(); ?>
+<div class="wrap">
+	<h2 class="page-title"><?php echo get_admin_page_title(); ?></h2>
+	<?php $wizard_names = Pronamic_WordPress_ConfigurationWizard_Factory::get_all_registered_wizards(); ?>
 
 
-<?php
+	<?php
 
-$ideal_basic_wizard = Pronamic_WordPress_ConfigurationWizard_Factory::get_wizard( 'idealbasic' );
+	$ideal_basic_wizard = Pronamic_WordPress_ConfigurationWizard_Factory::get_wizard( 'idealbasic' );
 
-$ideal_basic_wizard->show();
+	$ideal_basic_wizard->show();
 
-?>
+	?>
+</div>
